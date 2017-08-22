1f942d63ff6bdde50f591e51a437c8d761c42058
Arne Gevaert <arnegevaert@gmail.com>
2017-08-22 15:05:55 +0200

    Refactor

--- timeframe-length-converter.js
+++ timeframe-length-converter.js
@@ -1,18 +1,13 @@
 const fs = require('fs');
 const N3 = require('n3');
 const moment = require('moment');
+const util = require('./util.js');
 
-// Check arg count
-if (process.argv.length !== 5) {
-  console.log('Usage: node timeframe-length-converter.js LENGTH SOURCE DEST');
-  console.log('Provide length in minutes');
-}
+args = util.process_args(process.argv);
+const length = args.length;
+const source = args.source;
+const dest = args.dest;
 
-const length = process.argv[2];
-const source = process.argv[3];
-const dest = process.argv[4];
-
-check_args();
 convert_timeframe_length();
 
 function convert_timeframe_length() {
@@ -37,34 +32,30 @@ function convert_timeframe_length() {
 
     let reachedBorder = false;
     generatedAt.file.forEach(t => {
-      let ts = moment(get_timestamp_from_literal(t.object)).unix();
+      let ts = moment(util.get_timestamp_from_literal(t.object)).unix();
       if (start === -1) {
-        start = ts;
+        let rest = ts % (length * 60);
+        start = ts - rest;
         generatedAt.buffer.push(t);
-        get_triples_for_timestamp(t.object, measurements.file).forEach(t => {
+        util.get_triples_for_timestamp(t.object, measurements.file).forEach(t => {
           measurements.buffer.push(t);
         });
       } else if (ts - start < length*60) {
         generatedAt.buffer.push(t);
-        get_triples_for_timestamp(t.object, measurements.file).forEach(t => {
+        util.get_triples_for_timestamp(t.object, measurements.file).forEach(t => {
           measurements.buffer.push(t);
         });
       } else {
         reachedBorder = true;
         generatedAt.overflow.push(t);
-        get_triples_for_timestamp(t.object, measurements.file).forEach(t => {
+        util.get_triples_for_timestamp(t.object, measurements.file).forEach(t => {
           measurements.overflow.push(t);
         });
       }
     });
 
     if (reachedBorder) {
-      let i_out_filename = -1;
-      generatedAt.buffer.forEach(t => {
-        let ts = moment(get_timestamp_from_literal(t.object)).unix();
-        if (i_out_filename === -1 || i_out_filename > ts) i_out_filename = ts;
-      });
-      let out_filename = dest + i_out_filename.toString();
+      let out_filename = start.toString();
       console.log(out_filename);
       console.log('===============');
       measurements.buffer.forEach(t => console.log(t.graph, t.subject, t.predicate, t.object));
@@ -94,43 +85,4 @@ function convert_timeframe_length() {
       measurements.file = [];
     }
   }
-}
-
-function get_triples_for_timestamp(literal, triples) {
-  const ts = get_timestamp_from_literal(literal);
-  const result = [];
-  triples.forEach(t => {
-    if (get_timestamp_from_graph(t.graph) === ts) {
-      result.push(t);
-    }
-  });
-  return result;
-}
-
-function get_timestamp_from_graph(graph) {
-  return graph.substring(graph.length-19, graph.length);
-}
-
-function get_timestamp_from_literal(literal) {
-  return literal.substring(1, 20);
-}
-
-function check_args() {
-  fs.stat(source, (err, stats) => {
-    if (err) {
-      console.error('Directory does not exist: ' + source);
-      process.exit(1);
-    }
-    if (!stats.isDirectory) {
-      console.error(source + ' is not a directory');
-      process.exit(1);
-    }
-  });
-
-  fs.access(dest, fs.W_OK, (err) => {
-    if(err){
-      console.error(dest + ' is not a writable directory');
-      process.exit(1);
-    }
-  });
 }
