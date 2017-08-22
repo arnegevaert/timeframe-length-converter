0eb1cd690425b641028d39a2dbd56daf247eca02
Arne Gevaert <arnegevaert@gmail.com>
2017-08-22 13:14:37 +0200

    correctly splitting files in console log

--- timeframe-length-converter.js
+++ timeframe-length-converter.js
@@ -18,20 +18,81 @@ convert_timeframe_length();
 function convert_timeframe_length() {
   const filenames = fs.readdirSync(source).sort();
   const parser = N3.Parser();
-  console.log(filenames);
+
+  let start = -1;
+  let generatedAt = {file: [], buffer: [], overflow: []};
+  let measurements = {file: [], buffer: [], overflow: []};
   for (let i = 0; i < filenames.length; i++) {
     const filename = filenames[i];
     const raw = fs.readFileSync(source + filename, "utf8");
     const triples = parser.parse(raw);
-    const generatedAt = [];
-    const measurements = [];
-    triples.forEach((t) => {
+
+    triples.forEach(t => {
       if (t.predicate === 'http://www.w3.org/ns/prov#generatedAtTime') {
-        generatedAt.push(t);
+        generatedAt.file.push(t);
       } else {
-        measurements.push(t);
+        measurements.file.push(t);
       }
     });
+
+    let reachedBorder = false;
+    generatedAt.file.forEach(t => {
+      let ts = moment(get_timestamp_from_literal(t.object)).unix();
+      if (start === -1) {
+        start = ts;
+        generatedAt.buffer.push(t);
+        get_triples_for_timestamp(t.object, measurements.file).forEach(t => {
+          measurements.buffer.push(t);
+        });
+      } else if (ts - start < length*60) {
+        generatedAt.buffer.push(t);
+        get_triples_for_timestamp(t.object, measurements.file).forEach(t => {
+          measurements.buffer.push(t);
+        });
+      } else {
+        reachedBorder = true;
+        generatedAt.overflow.push(t);
+        get_triples_for_timestamp(t.object, measurements.file).forEach(t => {
+          measurements.overflow.push(t);
+        });
+      }
+    });
+
+    if (reachedBorder) {
+      let i_out_filename = -1;
+      generatedAt.buffer.forEach(t => {
+        let ts = moment(get_timestamp_from_literal(t.object)).unix();
+        if (i_out_filename === -1 || i_out_filename > ts) i_out_filename = ts;
+      });
+      let out_filename = dest + i_out_filename.toString();
+      console.log(out_filename);
+      console.log('===============');
+      measurements.buffer.forEach(t => console.log(t.graph, t.subject, t.predicate, t.object));
+      console.log();
+
+      // Serialize buffers to file
+
+      // Re-initialize buffers with overflow buffer content
+      generatedAt.file = generatedAt.overflow;
+      measurements.file = measurements.overflow;
+      generatedAt.buffer = [];
+      measurements.buffer = [];
+      generatedAt.overflow = [];
+      measurements.overflow = [];
+
+      console.log('overflow: generatedAt');
+      console.log('=====================');
+      generatedAt.file.forEach(t => console.log(t.graph, t.subject, t.predicate, t.object));
+      console.log('overflow: measurements');
+      console.log('======================');
+      measurements.file.forEach(t => console.log(t.graph, t.subject, t.predicate, t.object));
+      console.log();
+
+      start = -1;
+    } else {
+      generatedAt.file = [];
+      measurements.file = [];
+    }
   }
 }
 
