816ec4dbcb56ced9445fe24bafa9de11132b7a8c
Arne Gevaert <arnegevaert@gmail.com>
2017-08-22 10:26:52 +0200

    better argcheck

--- timeframe-length-converter.js
+++ timeframe-length-converter.js
@@ -1,15 +1,45 @@
+const fs = require('fs');
+
 // Check arg count
 if (process.argv.length !== 5) {
   console.log('Usage: node timeframe-length-converter.js LENGTH SOURCE DEST');
   console.log('Provide length in minutes');
 }
 
-// Check SOURCE readable and DEST writable
+const length = process.argv[2];
+const source = process.argv[3];
+const dest = process.argv[4];
 
-// While there is SOURCE data:
+check_args();
+convert_timeframe_length();
 
-//    Read LENGTH minutes of SOURCE data
+function convert_timeframe_length() {
+  // While there is SOURCE data:
 
-//    Determine filename of DEST file
+    // Read LENGTH minutes of SOURCE data
 
-//    Serialize data to DEST
+    // Determine filename of DEST file
+
+    // Serialize data to DEST
+}
+
+function check_args() {
+  // Check SOURCE readable and DEST writable
+  fs.stat(source, (err, stats) => {
+    if (err) {
+      console.error('Directory does not exist: ' + source);
+      process.exit(1);
+    }
+    if (!stats.isDirectory) {
+      console.error(source + ' is not a directory');
+      process.exit(1);
+    }
+  });
+
+  fs.access(dest, fs.W_OK, (err) => {
+    if(err){
+      console.error(dest + ' is not a writable directory');
+      process.exit(1);
+    }
+  });
+}
